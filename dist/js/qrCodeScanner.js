28809a8f969de848244933cccee63b65e4788538
RasangaAbeykoon <rasanga.2017595@iit.ac.lk>
2021-01-13 23:00:55 +0530

    QR

--- dist/js/qrCodeScanner.js
+++ dist/js/qrCodeScanner.js
@@ -1,64 +1,65 @@
 
-    qrcode = window.qrcode;
+qrcode = window.qrcode;
 
-    const video = document.createElement("video");
-    const canvasElement = document.getElementById("qr-canvas");
-    const canvas = canvasElement.getContext("2d");
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
 
-    const qrResult = document.getElementById("qr-result");
-    const outputData = document.getElementById("outputData");
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
 const btnScanQR = document.getElementById("btn-scan-qr");
-const contentBody = document.getElementsByClassName("background-container");
+const contentBody = document.getElementById("content_jobs");
 
-    let scanning = false;
+let scanning = false;
 
-        qrcode.callback = res => {
-            if (res) {
+qrcode.callback = res => {
+    if (res) {
         outputData.innerText = res;
-    scanning = false;
+        scanning = false;
 
-                video.srcObject.getTracks().forEach(track => {
-        track.stop();
-});
+        video.srcObject.getTracks().forEach(track => {
+            track.stop();
+        });
 
- $('#qr-result').popup("open");
-qrResult.hidden = false;
-canvasElement.hidden = true;
-                btnScanQR.hidden = false;
-                contentBody.hidden = false;
-}
+        $('#qr-result').popup("open");
+        qrResult.hidden = false;
+        canvasElement.hidden = true;
+        btnScanQR.hidden = false;
+        contentBody.hidden = false;
+    }
 };
 
-        btnScanQR.onclick = () => {
-        navigator.mediaDevices
-            .getUserMedia({ video: { facingMode: "environment" } })
-            .then(function (stream) {
-                scanning = true;
-                qrResult.hidden = true;
-                btnScanQR.hidden = true;
-                contentBody.hidden = true;
-                canvasElement.hidden = false;
-                video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-                video.srcObject = stream;
-                video.play();
-                tick();
-                scan();
-            });
+btnScanQR.onclick = () => {
+    navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: "environment" } })
+        .then(function (stream) {
+            scanning = true;
+            qrResult.hidden = true;
+            $('#qr-result').popup("close");
+            btnScanQR.hidden = true;
+            contentBody.hidden = true;
+            canvasElement.hidden = false;
+            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+            video.srcObject = stream;
+            video.play();
+            tick();
+            scan();
+        });
 };
 
-        function tick() {
-        canvasElement.height = video.videoHeight;
+function tick() {
+    canvasElement.height = video.videoHeight;
     canvasElement.width = video.videoWidth;
     canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
     scanning && requestAnimationFrame(tick);
 }
 
-        function scan() {
-            try {
+function scan() {
+    try {
         qrcode.decode();
-            } catch (e) {
+    } catch (e) {
         setTimeout(scan, 300);
-}
+    }
 }
 
